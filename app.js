e0e5d685cbe3b0ee744fb83810a9136d0a1092fc
Camille <gryspeerdt.camille@gmail.com>
2023-03-23 12:00:12 +0100

    s

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('https://newspaper-back.vercel.app/routes/index');
-var usersRouter = require('https://newspaper-back.vercel.app/routes/users');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
